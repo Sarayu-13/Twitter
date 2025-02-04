c681cf5911fcc068414f6ccf213cfbff5e294222
Sarayu-13 <138355499+Sarayu-13@users.noreply.github.com>
2025-02-04 15:57:51 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 10000;
 const { twitterClient } = require("./twitterClient.js")
 const CronJob = require("cron").CronJob;
 
